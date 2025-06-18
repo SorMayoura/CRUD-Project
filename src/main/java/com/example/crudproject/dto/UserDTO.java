b4e1ed3bdec6b8a7a767cef07bd2873560ca8193
SorMayoura <sormayoura@gmail.com>
2025-06-18 16:38:25 +0900

    feat: Add validation check

--- src/main/java/com/example/crudproject/dto/UserDTO.java
+++ src/main/java/com/example/crudproject/dto/UserDTO.java
@@ -1,8 +1,16 @@
 package com.example.crudproject.dto;
 
+import jakarta.validation.constraints.*;
+
 public class UserDTO {
     private long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min=2, max=50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
 
     public long getId() {
---
+++ src/main/java/com/example/crudproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.crudproject.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        Map<String, List<String>> errorResponse = Map.of("errors", errors);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
